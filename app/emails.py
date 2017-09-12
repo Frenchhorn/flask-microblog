3b10d32220ffd2230358e867a1e1db58071cc713
Frenchhorn <nyanpasspass@outlook.com>
2017-09-12 18:45:51 +0800

    Refactor send email feature

--- app/emails.py
+++ app/emails.py
@@ -3,18 +3,17 @@ from flask import render_template
 from flask_mail import Message
 from app import app, mail
 from config import ADMINS
-from .decorators import async
+from .decorators import thread
 
-@async
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
 
+@thread
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    send_async_email(app, msg)
+    with app.app_context():
+        mail.send(msg)
+
 
 def follower_notification(followed, follower):
     send_email("[microblog] %s is now following you!" % follower.nickname,
